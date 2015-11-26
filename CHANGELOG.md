870998014f04d82901548609f71267debc172b04
KiAnXineki <galerajimenez@gmail.com>
2015-11-26 09:57:11 +0000

    upgrade version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
 #CHANGELOG
+## 1.0.8 (2015-11-26)
+- update README.md
+
 ## 1.0.7 (2015-11-25)
 - Add documentation
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ changelog = read_md('CHANGELOG.md')
 
 setup(
     name='pywildcard',
-    version='1.0.7',
+    version='1.0.8',
     description='wildcard',
     long_description=readme+'\n\n'+changelog,
     author='Firecarrot',
